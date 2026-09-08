f8bf3eae3148d61fb5558517e35ff3f4896f77a5
mdjibran1337 <30729595+mdjibran1337@users.noreply.github.com>
2026-09-07 23:19:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-const { PeerServer } = require('peer.server');
+const { PeerServer } = require('peer');
 const peerServer = PeerServer({ port: process.env.PORT || 9000, path: '/' });
 console.log('BiteGo Custom PeerJS Server running...');
